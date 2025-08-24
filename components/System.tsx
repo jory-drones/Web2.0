6c58e75bcbee4c0f13be4eebb13db2c7626d3b59
jory-drones <jory@securitydrones.info>
2025-08-23 19:02:01 -0700

    Update System.tsx

--- components/System.tsx
+++ components/System.tsx
@@ -4,11 +4,13 @@ export default function System() {
     <section className="py-20 px-6 md:px-16 flex flex-col md:flex-row items-center justify-between gap-12">
       {/* Image */}
       <div className="flex-shrink-0 w-full md:w-1/2 flex justify-center">
-        <img
-          src="/assets/thesystem2.png"
-          alt="The System"
-          className="max-h-[350px] w-auto object-contain"
-        />
+       <Image
+  src="/assets/thesystem2.png"
+  alt="The System"
+  width={900}
+  height={600}
+  className="max-h-[350px] w-auto h-auto object-contain"
+/>
       </div>
 
       {/* Text */}
